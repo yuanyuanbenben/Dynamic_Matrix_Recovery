1045854b9fcd46b41cae1a5b56ac685780b84b0d
yuanyuanbenben <37060850+yuanyuanbenben@users.noreply.github.com>
2023-02-09 15:07:59 +0800

    Update help_functions.R

--- code/simulation/help_functions.R
+++ code/simulation/help_functions.R
@@ -1,3 +1,4 @@
+# some help functions
 
 # kernel function used in DLR method
 kernel_weight<- function(t,h,T_){
@@ -20,7 +21,7 @@ kernel_weight<- function(t,h,T_){
   }
 }
 
-# compare matrix difference
+# compare matrix difference for matrix M and M_ with dimension p*q
 compare_matrix_func <- function(M_,M,p,q){
   err_matrix <- M_-M
   mse <- norm(err_matrix,"F")^2/(p*q)
@@ -32,7 +33,7 @@ compare_matrix_func <- function(M_,M,p,q){
               "max_err_rate"=max_err_rate,"mean_err_rate"=mean_err_rate))
 }
 
-# compare using cv
+# compare using cv for estimated M with sample X1,X2,Y
 compare_cv_func <- function(M_,X1,X2,Y){
   l = length(X1)
   err = 0
@@ -41,23 +42,38 @@ compare_cv_func <- function(M_,X1,X2,Y){
   }
   return(err/l)
 }
-# simulation function
+
+#' DLR simulation function at a single time point t
+#' @param U_0,U_1,V_0,V_1,D_0,D_1: matrix->construct M(t)
+#' @param X1,X2,Y: array->samples with Y = M[X1,X2] + xi or FALSE-> random samples
+#' @param T_,n_t,t: int->total time points,  sample size in each time, current time
+#' @param p,q,r: int->dimensions and rank of underground truth M(t)
+#' @param h,lambda: num->bandwidth and tuning parameter
+#' @param M_input: matrix->initial matrix or FALSE->random initialization 
+#' @param X_rela,eps_rela: bool-> whether dependence exist for X and xi
+#' @param rela_para,auto_rela: num-> control dependent coefficients
+#' @param eps_sd:num->standard variance of xi
+#' @param eps_gene: matrix->dependent xi construction of FALSE-> random sturcture
+#' @param tor: num-> stop condition
+#' @return list->result_FISTA:MSE,ret_FISTA:estimated M(t):X1,X2,Y:sample used currently
 simulation_func_single <- function(U_0,U_1,V_0,V_1,D_0,D_1,
                                    X1=FALSE,X2=FALSE,Y=FALSE,T_=100,n_t=100,p=30,q=25,r=5,t=50,
-                                   h=9,X_input=FALSE,X_rela=FALSE,rela_para=FALSE,
+                                   h=9,X_rela=FALSE,rela_para=FALSE,
                                    eps_sd=0.1,
                                    M_input=FALSE,
                                    eps_rela=FALSE,auto_rela=FALSE,eps_gene=FALSE,
                                    lambda=1.5,tor=30){
-  # generate simulation data
-  # generate matrices M
   
+  # generate simulation data
+  
+  # generate matrices M(t)
   M <- (cos(pi*t/2/T_)*U_0 + sin(pi*t/2/T_)*U_1)%*%(D_1 + 
                                                       t/T_*D_0)%*%t(cos(pi*t/2/T_)*V_0 + sin(pi*t/2/T_)*V_1)
   
   
   # generate treatment X
   h_ = 2*as.integer(h/2)+1
+  # dependence of X exist
   if (X_rela){
     repl_num <- as.integer(rela_para*n_t)
     generate_rela_X_func <- function(index1,index2,repl_num){
@@ -68,6 +84,7 @@ simulation_func_single <- function(U_0,U_1,V_0,V_1,D_0,D_1,
       index2 <- c(index2[nondele_index],repl_index2)
       return(list(index1,index2))
     }
+    # start time t=1
     if (t==1) {
       X1 = array(0,dim = c(as.integer(h/2)+1,n_t))
       X2 = array(0,dim = c(as.integer(h/2)+1,n_t))
@@ -82,15 +99,18 @@ simulation_func_single <- function(U_0,U_1,V_0,V_1,D_0,D_1,
       }
     }
     else {
+      # time points that near begining
       if (t - as.integer(h/2) <= 1){
         index <- generate_rela_X_func(X1[t+as.integer(h/2)-1,],X2[t+as.integer(h/2)-1,],repl_num)
         X1 <- rbind(X1,index[[1]])
         X2 <- rbind(X2,index[[2]])
       }
+      # time points that near ending
       if (t + as.integer(h/2) > T_){
         X1 = X1[2:(as.integer(h/2) + T_ - t + 2),]
         X2 = X2[2:(as.integer(h/2) + T_ - t + 2),]
       }
+      # regular time points
       if (t - as.integer(h/2) > 1 & t + as.integer(h/2) <= T_) {
         index <- generate_rela_X_func(X1[h_,],X2[h_,],repl_num)
         X1 <- rbind(X1[2:h_,],index[[1]])
@@ -98,6 +118,7 @@ simulation_func_single <- function(U_0,U_1,V_0,V_1,D_0,D_1,
       }
     }
   }
+  # independent case for X 
   else{
     if (t==1) {
       X1 = array(0,dim = c(as.integer(h/2)+1,n_t))
@@ -128,7 +149,8 @@ simulation_func_single <- function(U_0,U_1,V_0,V_1,D_0,D_1,
       }
     }
   }
-  # generate Y
+  
+  # generate Y = M[X1,X2]
   if (t == 1) {
     Y = matrix(0,as.integer(h/2)+1,n_t)
     for (j in 1:(as.integer(h/2)+1)) {
@@ -143,7 +165,7 @@ simulation_func_single <- function(U_0,U_1,V_0,V_1,D_0,D_1,
     if (t - as.integer(h/2) <= 1){
       y = matrix(0,1,n_t)
       M_ <- (cos(pi*(t + as.integer(h/2))/2/T_)*U_0 + sin(pi*(t + as.integer(h/2))/2/T_)*U_1)%*%(D_1 + 
-                                                                                                   (t + as.integer(h/2))/T_*D_0)%*%t(cos(pi*(t + as.integer(h/2))/2/T_)*V_0 + sin(pi*(t + as.integer(h/2))/2/T_)*V_1)
+        (t + as.integer(h/2))/T_*D_0)%*%t(cos(pi*(t + as.integer(h/2))/2/T_)*V_0 + sin(pi*(t + as.integer(h/2))/2/T_)*V_1)
       for (i in 1:n_t) {
         y[1,i] <- M_[X1[(t + as.integer(h/2)),i],X2[(t + as.integer(h/2)),i]]
       }
@@ -155,13 +177,15 @@ simulation_func_single <- function(U_0,U_1,V_0,V_1,D_0,D_1,
     if (t - as.integer(h/2) > 1 & t + as.integer(h/2) <= T_){
       Y[1:(h_-1),] = Y[2:h_,]
       M_ <- (cos(pi*(t + as.integer(h/2))/2/T_)*U_0 + sin(pi*(t + as.integer(h/2))/2/T_)*U_1)%*%(D_1 + 
-                                                                                                   (t + as.integer(h/2))/T_*D_0)%*%t(cos(pi*(t + as.integer(h/2))/2/T_)*V_0 + sin(pi*(t + as.integer(h/2))/2/T_)*V_1)
+        (t + as.integer(h/2))/T_*D_0)%*%t(cos(pi*(t + as.integer(h/2))/2/T_)*V_0 + sin(pi*(t + as.integer(h/2))/2/T_)*V_1)
       for (i in 1:n_t) {
         Y[h_,i] <- M_[X1[h_,i],X2[h_,i]]
       }
     }
   }
   
+  # add noise xi for Y
+  # dependent case for xi
   if (eps_rela){
     resid_para <- sqrt(1-auto_rela**2)
     if (t==1){
@@ -184,6 +208,7 @@ simulation_func_single <- function(U_0,U_1,V_0,V_1,D_0,D_1,
       eps_gene <- auto_rela*eps_gene + resid_para*matrix(rnorm(p*q,sd=eps_sd),p,q)
     }
   }
+  #independent case for xi
   else{
     if (t==1){
       eps = matrix(rnorm((1+as.integer(h/2))*n_t),1+as.integer(h/2),n_t)
@@ -193,6 +218,8 @@ simulation_func_single <- function(U_0,U_1,V_0,V_1,D_0,D_1,
       Y[min(t+as.integer(h/2),h_),] <- Y[min(t+as.integer(h/2),h_),] + rnorm(n_t,sd=eps_sd)
     }
   }
+  
+  
   if (t==1){
     M_init <- matrix(0,p,q)
     for (i in 1:n_t) {
@@ -205,7 +232,6 @@ simulation_func_single <- function(U_0,U_1,V_0,V_1,D_0,D_1,
   }
   #svd_ret_FISTA <- svd(ret_FISTA)
   #M_FISTA <- svd_ret_FISTA$u[,1:r]%*%diag(svd_ret_FISTA$d[1:r])%*%t(svd_ret_FISTA$v[,1:r])
-  
   # cv_x
   # x1 <- sample(1:p,round(n_t/5),replace = TRUE)
   # x2 <- sample(1:q,round(n_t/5),replace = TRUE)
@@ -221,8 +247,15 @@ simulation_func_single <- function(U_0,U_1,V_0,V_1,D_0,D_1,
   return(list(result_FISTA,ret_FISTA,X1,X2,Y))
 }
 
-
-simulation_func_baseline <- function(U_0,U_1,V_0,V_1,D_0,D_1,T_=100,n_t=100,p=30,q=25,r=5,t=50,eps_sd=0.1,lambda=1.5){
+#' simulation functon for Static
+#' @param U_0,U_1,V_0,V_1,D_0,D_1: matrix->construct M(t)
+#' @param X1,X2,Y: array->samples with Y = M[X1,X2] + xi or FALSE-> random samples
+#' @param T_,n_t,t: int->total time points,  sample size in each time, current time
+#' @param p,q,r: int->dimensions and rank of underground truth M(t)
+#' @param lambda: num-> tuning parameter
+#' @param eps_sd:num->standard variance of xi
+#' @return list-> MSE, estimated M(t)
+simulation_func_baseline <- function(U_0,U_1,V_0,V_1,D_0,D_1,X1=FALSE,X2=FALSE,Y=FALSE,T_=100,n_t=100,p=30,q=25,r=5,t=50,eps_sd=0.1,lambda=1.5){
   M <- (cos(pi*t/2/T_)*U_0 + sin(pi*t/2/T_)*U_1)%*%(D_1 + 
                                                       t/T_*D_0)%*%t(cos(pi*t/2/T_)*V_0 + sin(pi*t/2/T_)*V_1)
   X1 <- sample(1:p,n_t,replace = TRUE)
@@ -233,11 +266,6 @@ simulation_func_baseline <- function(U_0,U_1,V_0,V_1,D_0,D_1,T_=100,n_t=100,p=30
   }
   Y <- Y + rnorm(n_t,sd = eps_sd)
   M_init <- matrix(0,p,q)
-  #for (i in 1:n_t) {
-  #  M_init[X1[i],X2[i]] <- Y[i]
-  #}
   result_baseline <- baseline_FISTA_func(X1,X2,Y,n_t,p,q,lambda=lambda,init = TRUE,M_input = M_init)
-  #print(M-result_baseline)
-  #return(result_baseline)
-  return(compare_matrix_func(result_baseline,M,p,q))
+  return(list(compare_matrix_func(result_baseline,M,p,q),result_baseline))
 }
