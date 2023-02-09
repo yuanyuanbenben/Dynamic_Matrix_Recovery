87d65ebd09ccba64b39f6e0b416dc823abe9caa6
yuanyuanbenben <37060850+yuanyuanbenben@users.noreply.github.com>
2023-02-09 15:28:17 +0800

    Update help_functions.R

--- code/simulation/help_functions.R
+++ code/simulation/help_functions.R
@@ -269,3 +269,49 @@ simulation_func_baseline <- function(U_0,U_1,V_0,V_1,D_0,D_1,X1=FALSE,X2=FALSE,Y
   result_baseline <- baseline_FISTA_func(X1,X2,Y,n_t,p,q,lambda=lambda,init = TRUE,M_input = M_init)
   return(list(compare_matrix_func(result_baseline,M,p,q),result_baseline))
 }
+
+# from results of Static method to TwoStep method
+# using an additional local smooth
+
+createdata_func <- function(matrix_array,t,T_,h){
+  h_ = as.integer(h/2)
+  
+  if (t - h_ <= 1){
+    m <- matrix_array[1:(h_+t),,]
+  }
+  if (t + h_ > T_){
+    m <- matrix_array[(t-h_):T_,,]
+  }
+  if (t - h_ > 1 & t + h_ <= T_) {
+    m <- matrix_array[(t-h_):(t+h_),,]
+  }
+  return(m)
+}
+
+threshold_func <- function(x) sapply(x, function(z) max(0,z))
+
+# local smooth 
+local_lowrank_smooth <- function(matrix_array,T_,t,h,p,q,lambda){
+  weight <- kernel_weight(t,h,T_)
+  weight <- weight/sum(weight)
+  M <- matrix(0,p,q)
+  len <- dim(matrix_array)[1]
+  for (i in 1:len) {
+    M <- M + weight[i]*matrix_array[i,,]
+  }
+  svd_M <- svd(M)
+  N <- svd_M$u%*%diag(threshold_func(svd_M$d-lambda))%*%t(svd_M$v)
+  return(N)
+}
+
+local_smooth <- function(matrix_array,T_,t,h,p,q,lambda){
+  weight <- kernel_weight(t,h,T_)
+  weight <- weight/sum(weight)
+  M <- matrix(0,p,q)
+  len <- dim(matrix_array)[1]
+  for (i in 1:len) {
+    M <- M + weight[i]*matrix_array[i,,]
+  }
+  return(M)
+}
+                                     
