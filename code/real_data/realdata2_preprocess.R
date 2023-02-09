996a9bf17bbdb3738d212961d97aa87560e82bba
yuanyuanbenben <37060850+yuanyuanbenben@users.noreply.github.com>
2023-02-09 21:47:19 +0800

    Update realdata2_preprocess.R

--- code/real_data/realdata2_preprocess.R
+++ code/real_data/realdata2_preprocess.R
@@ -23,19 +23,6 @@ T_ = dim(img_total_b)[1]
 p = dim(img_total_b)[2]
 q = dim(img_total_b)[3]
 
-# show jpeg
-# test 
-# par(mar=c(0,1,0,1))
-# img <- array(0,dim = c(480,854,3))
-# img[,,1] <- twostep_red[1,,]#img_total_r[85,,]
-# img[,,2] <- twostep_green[1,,]#img_total_g[85,,]
-# img[,,3] <- twostep_blue[1,,]#img_total_b[85,,]
-# longImage<-melt(img)
-# rgbImage<-reshape(longImage,timevar='Var3',idvar=c('Var1','Var2'),direction='wide')
-# rgbImage$Var1<- -rgbImage$Var1
-# colorColumns<- rgbImage[, substr(colnames(rgbImage), 1, 5)== "value"]
-# with(rgbImage,plot(Var2, Var1, col = rgb(colorColumns), asp = 1, pch =".",axes=F,xlab='',ylab=''))
-
 # do RPCA to decomposition M to L and S
 L_total = array(0,dim = c(3,T_,p,q))
 S_total = array(0,dim = c(3,T_,p,q))
