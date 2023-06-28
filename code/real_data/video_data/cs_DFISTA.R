8f493efc40671ffbf89e9457f6fe0c9b6a9ee4fb
陈自元 <2101110075@pku.edu.cn>
2023-06-29 00:39:37 +0800

    Add files via upload

---
+++ code/real_data/video_data/cs_DFISTA.R
@@ -0,0 +1,139 @@
+# using FISTA algorithm for compressed sensing problem
+# DLR method
+# real data example 2: compress and recover the lion video from Davis dataset 
+
+# kernel 
+kernel_weight<- function(t,h,T_){
+  epan_ker <- function(x,h){
+    if (abs(x) > as.integer(h/2)){
+      return(0)
+    }
+    else {
+      #return(3/4*(1-4*x^2/h^2))
+      return((as.integer(h/2)+1-abs(x))^3*4.95/45)
+    }
+  }
+  if (t - as.integer(h/2) <= 1){
+    return(sapply((-t+1):as.integer(h/2), epan_ker,h))
+  }
+  if (t + as.integer(h/2) >= T_){
+    return(sapply(as.integer(-h/2):(T_-t), epan_ker,h))
+  }
+  if (t -  as.integer(h/2) > 1 & t + as.integer(h/2) < T_) {
+    return(sapply(as.integer(-h/2):as.integer(h/2), epan_ker,h))
+  }
+}
+
+
+# object function
+inner_pro_func_M <- function(M,X1,X2,Y,len,n,d,v,minibatch,batch_size,conv_ker){
+  # X T*n_t*p*q
+  # M p*q
+  ret <- rep(0,batch_size)
+  for (index in 1:batch_size) {
+    j <- minibatch[1,index]
+    i <- minibatch[2,index]
+    s <- X1[i,j]
+    t <- X2[i,j]
+    ret[index] <- sum(M[c(s-1,s,s+1),c(t-1,t,t+1)]*conv_ker) - Y[i,j]
+  }
+  return(ret)
+}
+
+obj_func  <- function(M,inner_pro,minibatch,batch_size,weight,lambda){
+  # weight_mat T*T
+  # Y, inner_pro T*n_t
+  obj <- 0
+  for (index in 1:batch_size) {
+    obj <- obj + weight[minibatch[1,index]]*(inner_pro[index]^2)
+  }
+  return(obj + lambda*nuclear(M))
+}
+
+# gradient function
+grad_func <- function(X1,X2,inner_pro,minibatch,batch_size,d,v,weight,p,q,conv_ker){
+  # weight_mat T*T
+  # Y, inner_pro T*n_t
+  # X T*n_t*p*q
+  grad_mat <- matrix(0,p,q)
+  for (index in 1:batch_size) {
+    j <- minibatch[1,index]
+    i <- minibatch[2,index]
+    s <- X1[i,j]
+    t <- X2[i,j]
+    grad_mat[c(s-1,s,s+1),c(t-1,t,t+1)] <- grad_mat[c(s-1,s,s+1),c(t-1,t,t+1)] + weight[j]*inner_pro[index]*conv_ker
+  }
+  return(2*grad_mat)
+}
+
+
+sample_func <- function(batch_size,weight,len,n){
+  size1 <- sample(1:len,batch_size,replace = TRUE)#,prob = diag(weight_mat))
+  size2 <- sample(1:n,batch_size,replace = TRUE)
+  return(rbind(size1,size2))
+}
+
+# compute Lipschitz constant
+Lipschitz_func <- function(X1,X2,len,n,weight,p,q,conv_ker){
+  L_mat <- matrix(0,p,q)
+  for (j in 1:len) {
+    for (i in 1:n) {
+      s = X1[i,j]
+      t = X2[i,j]
+      L_mat[c(s-1,s,s+1),c(t-1,t,t+1)] <- L_mat[c(s-1,s,s+1),c(t-1,t,t+1)] + weight[j]*conv_ker
+    }
+  }
+  return(2*6*norm(L_mat,type = "F"))
+}
+
+# main function
+cs_FISTA_func <- function(X1,X2,Y,T_,t_,h,p,q,lambda,conv_ker,itertime=35000,sto=FALSE,
+                          batch_size=FALSE,init=TRUE,M_input=FALSE,
+                          eta=1.2,constant=TRUE,tor=100){
+  # initial
+  if (t_ == 1) {
+    itertime = 20000
+  }
+  # if (t_ < h/2) {
+  #   tor = tor/2
+ #  }
+  n = dim(X1)[1]
+  len = dim(X1)[2]
+  weight <- kernel_weight(t_,h,T_)
+  L <- Lipschitz_func(X1,X2,len,n,weight,p,q,conv_ker)
+  print(L)
+  t = 1
+  if (init){
+    M = matrix(rnorm(p*q),p,q)
+  }
+  else{
+    M = M_input
+  }
+  N <- M
+  #iteration
+  obj_value_before <- Inf
+  for (iter in 1:itertime) {
+    # print(iter)
+    minibatch <- sample_func(batch_size,weight,len,n)
+    inner_pro <- inner_pro_func_M(N,X1,X2,Y,len,n,d,v,minibatch,batch_size,conv_ker)
+    grad_N <- grad_func(X1,X2,inner_pro,minibatch,batch_size,d,v,weight,p,q,conv_ker)
+    svd_G <- svd(N - 1/L*grad_N)
+    M_ <- svd_G$u%*%diag(threshold_func(svd_G$d,lambda/L))%*%t(svd_G$v)
+    t_ <- (1+sqrt(1+4*t^2))/2
+    N <- M + (t-1)/t_*(M_-M)
+    M <- M_
+    t <- t_
+    if (as.integer(iter/20)*20==iter){
+      print(iter)
+      obj_value <- obj_func(M,inner_pro,minibatch,batch_size,weight,lambda)
+      print(obj_value)
+      if (abs(obj_value-obj_value_before) < tor) break
+      obj_value_before <- obj_value
+      N[N>1]=0.98
+      N[N<0]=0.02
+      M[M>1]=0.98
+      M[M<0]=0.02
+    }
+  }
+  return(M)
+}
---
+++ code/real_data/video_data/realdata2_test.R
@@ -0,0 +1,89 @@
+# real data 2
+# DIVIS 2017 videos
+
+# environment
+library(kernlab)
+library(psych)
+library(ggplot2)
+library(reshape2)
+library(foreach)
+library(doParallel)
+library(npmr)
+
+
+setwd('/home/yuanyuanbenben/project_dmc/dmr_github/Dynamic_Matrix_Recovery-main')
+
+
+source("code/real_data/cs_DFISTA.R")
+source("code/real_data/cs_baseline_FISTA.R")
+source("code/real_data/help_functions.R")
+source("code/real_data/video_data/robust_pca.R")
+
+load("code/real_data/video_data/lions_video.RData")
+
+cl.cores = detectCores(logical = F)
+cl <- makeCluster(21)
+registerDoParallel(cl)
+
+conv_ker = 1/16*matrix(c(1,2,1,2,4,2,1,2,1),3,3)
+
+set.seed(378461087)
+
+# compress step
+n = 60000
+h = 5
+X1_total <- array(sample(2:(p-1),n*T_,replace = TRUE),dim = c(n,T_))
+X2_total <- array(sample(2:(q-1),n*T_,replace = TRUE),dim = c(n,T_))
+Y_total <- array(0,dim = c(3,n,T_))
+for (i in 1:T_) {
+  for (j in 1:n) {
+    s = X1_total[j,i]
+    t = X2_total[j,i]
+    Y_total[1,j,i] <- sum(L_total[1,i,c(s-1,s,s+1),c(t-1,t,t+1)]*conv_ker)
+    Y_total[2,j,i] <- sum(L_total[2,i,c(s-1,s,s+1),c(t-1,t,t+1)]*conv_ker)
+    Y_total[3,j,i] <- sum(L_total[3,i,c(s-1,s,s+1),c(t-1,t,t+1)]*conv_ker)
+  }
+}
+
+#
+# DLR method
+#
+batch_size = as.integer(n)
+lambda = 2.75
+tor = 0
+label = c("red","green","blue")
+result_mse <- foreach(mode=c(1,2,3),.packages = c("psych","kernlab","npmr"),.verbose=TRUE)%:%
+  foreach(t = c(5,25,45,65,85),.packages = c("psych","kernlab","npmr"),.verbose=TRUE) %dopar% {
+    datas <- cs_realdata_createdata_func(t,X1_total,X2_total,Y_total,T_,h,mode)
+    X1 <- datas[[1]]
+    X2 <- datas[[2]]
+    Y <- datas[[3]]
+    M_input <- matrix(rnorm(p*q),p,q)
+    M_input <- cs_FISTA_func(X1,X2,Y,T_,t,p,q,lambda,conv_ker,itertime=40000,sto=TRUE,
+                           batch_size=batch_size,init=FALSE,M_input=M_input,tor=tor)
+    
+  write.csv(construc_pic(t,M_input,mode,S_total),paste("output/lions/lions_",label[mode],"_",t,".csv",sep = ""))
+  cs_test_error_func(M_input,L_total[mode,t,,],p,q)
+}
+
+# 
+# Static method
+#
+batch_size = as.integer(n/5)
+lambda = 0.224
+tor=0
+label = c("red","green","blue")
+result_mse <- foreach(mode=c(1,2,3),.packages = c("psych","kernlab","npmr"),.verbose=TRUE)%:%
+  foreach(t = c(5,25,45,65,85),.packages = c("psych","kernlab","npmr"),.verbose=TRUE) %dopar% {
+    X1 <- X1_total[,t]
+    X2 <- X2_total[,t]
+    Y <- Y_total[mode,,t]
+    M_input <- matrix(rnorm(p*q),p,q)
+    M_input <- baseline_cs_FISTA_func(X1,X2,Y,T_,t,p,q,lambda,conv_ker,itertime=40000,sto=TRUE,
+                           batch_size=batch_size,init=FALSE,M_input=M_input,tor=tor)
+
+  write.csv(construc_pic(t,M_input,mode,S_total),paste("output/lions/baseline_lions_",label[mode],"_",t,".csv",sep = ""))
+  cs_test_error_func(M_input,L_total[mode,t,,],p,q)
+}
+
+stopCluster(cl)
